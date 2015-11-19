b2d59173fd239b0294d1c1c6e0012eca517c3ca7
przemyslaw-kublin <przemyslaw.kublin@thestory.pl>
2015-11-19 11:38:16 +0100

    Module configs

--- src/BraintreePayments/Module.php
+++ src/BraintreePayments/Module.php
@@ -5,17 +5,6 @@ use Zend\ModuleManager\Feature\AutoloaderProviderInterface;
 
 class Module implements AutoloaderProviderInterface
 {
-    public function getAutoloaderConfig()
-    {
-        return array(
-            'Zend\Loader\StandardAutoloader' => array(
-                'namespaces' => array(
-                    __NAMESPACE__ => __DIR__ . '/src/' . str_replace('\\', '/', __NAMESPACE__),
-                ),
-            ),
-        );
-    }
-
     public function getConfig()
     {
         return include __DIR__ . '/../../config/module.config.php';
